ac962277f569b3704e5096b873cba477d132a49d
Sergei Erjemin <seremin@msk.rsvo.ru>
2019-10-08 17:34:21 +0300

    rules firewall (2 continue)

--- 01-network-and-security/05-rules-of-firewall.sh
+++ 01-network-and-security/05-rules-of-firewall.sh
@@ -6,30 +6,86 @@
 # Вы должны установить правила брандмауэра на своем сервере только
 # с сервисами, используемыми вне виртуальной машины.
 
-echo "   ------------------------------------------------------------------";
-echo "Проверять порты открытые на сервере позволяет пакет 'net-tools'.";
-echo "Установить его можно командой:";
+echo "ЭТОТ СКРИПТ НУЖНО ЗАПУСКАТЬ С ПРАВАМИ АДМИНИСТРАТОРА (ИЗ ПОД SUDO).";
 echo "";
-echo "    sudo -S apt-get install net-tools";
-echo "";
-echo "Теперь с помощью команды 'netstat' можно просмотреть открытые порты";
-echo "и сокеты, а так же использующие их приложения. Например, открытые";
-echo "порты проверяются так:";
-echo "";
-echo "    netstat -nlp";
-echo "";
-echo "Если вы все делали праивильно, то на данный момент у вас открыт только";
-echo "один порт 2002 (это перенастроенный нами SSH):";
-echo "";
-echo "  tcp        0      0 0.0.0.0:2002            0.0.0.0:*               LISTEN      -";
-echo "  tcp6       0      0 :::2002                 :::*                    LISTEN      -";
-echo "";
-echo "В дальнейшем, все устанавливаемые пакеты и запущенные демоны сами";
-echo "откроют для себя нужные порты, будут менять их при измении настроек,";
-echo "и всё побудет хорошо.";
-echo "";
-# При желании, можно дополнительно настроить блокировки с помощью
-# команд route.
+
+# Установим пакет iptables (если он уже установлен, попытка повторной уствновки "проскочит"
+sudo -S apt-get install iptables
+sudo -S apt-get install iptables-persistent
+
+# Объявление переменных
+export IPT  ="/usr/sbin/iptables"
+export WAN  ="$(ip add | grep 'BROADCAST' |  awk '{print $2}' | cut -d ':' -f 1)"
+export WAN_IP   ="$(ip ad | grep 'inet ' | awk '(NR == 2)' | awk '{print $2}' | cut -d '/' -f1)"
+export WAN_IP6  ="$(ip ad | grep 'inet6 ' | awk '(NR == 2)' | awk '{print $2}' | cut -d '/' -f1)"
+
+# Очистка всех цепочек iptables
+$IPT -F
+$IPT -F -t nat
+$IPT -F -t mangle
+$IPT -X
+$IPT -t nat -X
+$IPT -t mangle -X
+
+# Установим политики по умолчанию (DROP) для трафика, не соответствующего
+# ни одному из правил
+$IPT -P INPUT DROP
+$IPT -P OUTPUT DROP
+$IPT -P FORWARD DROP
+
+# Разрешаем локальный траффик для loopback
+$IPT -A INPUT -i lo -j ACCEPT
+$IPT -A OUTPUT -o lo -j ACCEPT
+
+# Разрешаем пинги
+$IPT -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
+$IPT -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
+$IPT -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
+$IPT -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+# Разрешаем исходящие соединения самого сервера
+$IPT -A OUTPUT -o $WAN -j ACCEPT
+
+# Состояние ESTABLISHED говорит о том, что это не первый пакет в соединении.
+# Пропускать все уже инициированные соединения, а также дочерние от них.
+# Так мы разрешим использование текущего порта на который настроен и
+# открыт, в настощий момент, SSH.
+$IPT -A INPUT -p all -m state --state ESTABLISHED,RELATED -j ACCEPT
+# Разрешить новые, а так же уже инициированные и их дочерние соединения
+$IPT -A OUTPUT -p all -m state --state ESTABLISHED,RELATED -j ACCEPT
+# Разрешить форвардинг для уже инициированных и их дочерних соединений
+$IPT -A FORWARD -p all -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Включаем фрагментацию пакетов. Необходимо из-за разных значений MTU
+$IPT -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+
+# Отбрасывать все пакеты, которые не могут быть идентифицированы
+# и поэтому не могут иметь определенного статуса.
+$IPT -A INPUT -m state --state INVALID -j DROP
+$IPT -A FORWARD -m state --state INVALID -j DROP
+
+# Приводит к связыванию системных ресурсов, так что реальный
+# обмен данными становится не возможным, обрубаем
+$IPT -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+$IPT -A OUTPUT -p tcp ! --syn -m state --state NEW -j DROP
+
+# Открываем порт для SSH на тот случай, если скрипт будет запущен
+# не из терминала по SSH, а непосредственно из консоли виртуального
+# серверв (!!!не забудьте указать свой порт, который вы указали для SSH ранее!!!)
+$IPT -A INPUT -i $WAN -p tcp --dport 2002 -j ACCEPT
+# Открываем порт для web сервера (для http)
+$IPT -A INPUT -i $WAN -p tcp --dport 80 -j ACCEPT
+# Открываем порт для web сервера (для https)
+$IPT -A INPUT -i $WAN -p tcp --dport 443 -j ACCEPT
+
+# Записываем правила в файл
+/sbin/iptables-save > /etc/iptables_rules
+
+# sudo apt-get install iptables-persistent
+
+## Step 8
+# Execute iptables.sh script to configure iptables rules
+# ./iptables.sh
 
 # apt-get install iptables
 # /usr/sbin/iptables -?
--- README.md
+++ README.md
@@ -15,3 +15,23 @@
 2. Включаем для него sudo ([скрипт A](01-network-and-security/02a-sudo.sh), затем [скрипт B](01-network-and-security/02b-sudo.sh))
 3. Настроить сетевой интрефейс -- убрать dhcp и установить сетевую маску ([скрпит](01-network-and-security/03-make-DHCP-of_and-set-mask.sh))
 4. Сделать   SSH-доступ с RSA-ключами, перенастроить SSH-порты сервера и запретить логин root через SSH ([скрипт для сервера](01-network-and-security/04a-set-SSH-port-and-lock-root-login.sh) и [скрипт для клиента](01-network-and-security/04b-set-SSH-RSA-security-keys.sh) )
+
+
+-- У олега
+- VARIOUS ARTISTS	TWIN PEAKS (LIMITED EVENT SERIES SOUNDTRACK):SCORE (2LP)	0081227933951	2017	1750	1			
+
+
+-- У юрия
+- CRANBERRIES IN THE END (Cranberry) - 1 990
+- Massive Attack: Mezzanine (The Mad Professor Remixes) LP coloured vinyl - 1 690
+- Once Upon a Time in Hollywood (Quentin Tarantino) Original Motion Picture Soundtrack 2LP
+ПРЕДЗАКАЗ!!! Плановая дата релиза: 25.10.2019 TBC
+Black Vinyl - 1590 руб.
+Limited Orange Vinyl - 1690 руб.
+- Pink Floyd: The Later Years 1987-2019
+  ПРЕДЗАКАЗ!!! В продаже с 29 ноября 2019 года!
+  CD Digisleeve - Баркод: 0190295378295 - 1050 руб.
+  2LP 180 Gram Black Vinyl/Gatefold/Booklet - 0190295378288 - 2 550
+  !!!
+  
+  ПРЕДЛОЖЕНИЕ ТОЛЬКО ПО ЭТОЙ РАССЫЛКЕ, КОЛИЧЕСТВО ОГРАНИЧЕНО!
